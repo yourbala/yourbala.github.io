ec62debd2d8a077e6e0f482ef3e8b50bbe66d981
anandshankarbala <anandshankarbala@gmail.com>
2020-05-17 01:59:10 +0530

    comments removed

--- src/App.js
+++ src/App.js
@@ -43,39 +43,7 @@ function App() {
   return (
     <div className="App">
       <Menu homeUrl={HOME_URL} />
-      {/*
-  <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    <div className="container">
-      <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-      <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i className="fa fa-bars"></i>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarResponsive"
       
-      onClick={ () => this.setState({navbarMenuActive: !this.state.navbarMenuActive}) }>
-        <ul className="navbar-nav text-uppercase ml-auto">
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>  
-*/}
-
       <header className="masthead">
         <div className="container">
           <div className="intro-text">
@@ -212,15 +180,7 @@ function App() {
       <section className="py-5">
         <div className="container">
           <div className="row">
-            <div className="col-md-3 col-sm-6">
-              <a href="#">
-                <img
-                  className="img-fluid d-block mx-auto"
-                  src="img/logos/envato.jpg"
-                  alt="Smiley face"
-                />
-              </a>
-            </div>
+            
             <div className="col-md-3 col-sm-6">
               <a href="#">
                 <img
