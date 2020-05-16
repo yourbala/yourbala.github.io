c2259fd3f48322269313872696e5efeff5927205
anandshankarbala <anandshankarbala@gmail.com>
2020-05-16 18:55:19 +0530

    Updates

---
+++ precache-manifest.f1140d3844cfd3c5fc0bb9d3a84dc8f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905171b3120b5c1ce8a6f1dbbbd1d0b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa98eff3755668da94d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1199afcae8100f591b70",
+    "url": "/static/js/2.b8d6fa4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b8d6fa4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa98eff3755668da94d",
+    "url": "/static/js/main.cb63e2a1.chunk.js"
+  },
+  {
+    "revision": "ce166e8de4f3a5aee973",
+    "url": "/static/js/runtime-main.17aec7a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
